26736624eef1061955fa59f39c01e2b5f0e37fd0
venky2809 <jvenkatesh494@gmail.com>
2025-10-22 14:12:10 +0100

    Update default values for AWS variables

--- variables.tf
+++ variables.tf
@@ -1,10 +1,10 @@
 variable "aws_region" {
   description = "The AWS region to create things in."
-  default     = "us-east-1"
+  default     = "us-west-2"
 }
 variable "key_name" {
   description = " SSH keys to connect to ec2 instance"
-  default     = "jenkmins"
+  default     = "jenkins"
 }
 variable "instance_type" {
   description = "instance type for ec2"
@@ -12,12 +12,12 @@ variable "instance_type" {
 }
 variable "ami_id" {
   description = "AMI for Ubuntu Ec2 instance"
-  default     = "ami-0360c520857e3138f"
+  default     = "ami-046c2381f11878233"
 }
 variable "bucket_name" {
   description = "The name of the S3 bucket to create"
   type        = string
-  default     = "venkymaluma1234"
+  default     = "venkymaluma"
 }
 
 variable "acl" {
