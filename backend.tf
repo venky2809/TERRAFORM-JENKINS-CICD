db32962b63ea4499cf3d6335ce988a84b8399060
venky2809 <jvenkatesh494@gmail.com>
2025-10-22 14:04:44 +0100

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "s3" {
-    bucket         = "mrcloudbook-cicd-bucket"
+    bucket         = "venkymaluma"
     key            = "my-terraform-environment/main"
-    region         = "ap-south-1"
+    region         = "eu-west-2"
     dynamodb_table = "mrcloudbook-dynamo-db-table"
   }
 }
