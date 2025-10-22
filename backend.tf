294532f1f5f08a7837f4c7b3c4f96990582854fc
venky2809 <jvenkatesh494@gmail.com>
2025-10-22 14:06:40 +0100

    Update S3 bucket name in backend configuration

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "venkymaluma1234"
+    bucket         = "venkymaluma"
     key            = "my-terraform-environment/main"
     region         = "us-east-1"
     dynamodb_table = "mrcloudbook-dynamo-db-table"
